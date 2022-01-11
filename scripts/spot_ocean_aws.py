a07422926941dc4825956857762bba889df18d5a
Steven Feltner <steven.feltner@netapp.com>
2022-01-10 16:40:30 -0800

    Add debug flag to function

--- scripts/spot_ocean_aws.py
+++ scripts/spot_ocean_aws.py
@@ -3,15 +3,16 @@ import json
 import requests
 import os
 
-#from spotinst_sdk2 import SpotinstSession
+
+# from spotinst_sdk2 import SpotinstSession
 
 
 @click.group()
 @click.pass_context
 def cli(ctx, *args, **kwargs):
     ctx.obj = {}
-    #session = SpotinstSession()
-    #ctx.obj['client'] = session.client("ocean_aws")
+    # session = SpotinstSession()
+    # ctx.obj['client'] = session.client("ocean_aws")
 
 
 @cli.command()
@@ -19,11 +20,12 @@ def cli(ctx, *args, **kwargs):
 @click.argument('clusterid', )
 @click.argument('accountid', )
 @click.argument('token', )
+@click.option('--debug', default=False, type=bool)
 @click.pass_context
-def add(ctx, *args, **kwargs):
+def add(debug, *args, **kwargs):
     """Add a LoadBalancer to existing Ocean Cluster"""
 
-    loadBalancerarn = str(kwargs.get('loadbalancerarn'))
+    loadbalancerarn = str(kwargs.get('loadbalancerarn'))
     cluster_id = str(kwargs.get('clusterid'))
     account_id = str(kwargs.get('accountid'))
     token = str(kwargs.get('token'))
@@ -41,27 +43,30 @@ def add(ctx, *args, **kwargs):
             print('Status ', r.status_code)
             print(data)
         else:
+            loadbalancers = []
             items = data['response']['items']
             for x in items:
-                loadBalancers = x['compute']['launchSpecification']['loadBalancers']
-            #print(loadBalancers)
-            loadBalancers.append({'arn': loadBalancerarn, 'type': 'TARGET_GROUP'})
-            #print(loadBalancers)
+                loadbalancers = x['compute']['launchSpecification']['loadBalancers']
+            if debug:
+                print("Ocean LoadBalancer List: ", loadbalancers)
+            loadbalancers.append({'arn': loadbalancerarn, 'type': 'TARGET_GROUP'})
+            if debug:
+                print("New appended Ocean LoadBalancer List: " ,loadbalancers)
             headers = {
                 'Content-Type': 'application/json',
                 'Authorization': 'Bearer ' + token
             }
             url = 'https://api.spotinst.io/ocean/aws/k8s/cluster/' + cluster_id + '?accountId=' + account_id
-            data = {"cluster":{"compute":{"launchSpecification":{"loadBalancers":loadBalancers}}}}
+            data = {"cluster": {"compute": {"launchSpecification": {"loadBalancers": loadbalancers}}}}
 
             try:
                 r = requests.put(url, headers=headers, json=data)
                 data = json.loads(r.text)
                 if r.status_code != 200:
-                    print('Status ', r.status_code)
+                    print('Status: ', r.status_code, ' Failed to add LoadBalancer')
                     print(data)
                 else:
-                    print('Status ', r.status_code)
+                    print('Status: ', r.status_code, ' Successfully added LoadBalancer to Ocean cluster')
             except Exception as e:
                 print(e)
     except Exception as e:
@@ -73,10 +78,11 @@ def add(ctx, *args, **kwargs):
 @click.argument('clusterid', )
 @click.argument('accountid', )
 @click.argument('token', )
+@click.option('--debug', default=False, type=bool)
 @click.pass_context
-def delete(ctx, *args, **kwargs):
+def delete(debug, *args, **kwargs):
     """Delete a LoadBalancer from existing Ocean Cluster"""
-    loadBalancerarn = str(kwargs.get('loadbalancerarn'))
+    loadbalancerarn = str(kwargs.get('loadbalancerarn'))
     cluster_id = str(kwargs.get('clusterid'))
     account_id = str(kwargs.get('accountid'))
     token = str(kwargs.get('token'))
@@ -94,33 +100,36 @@ def delete(ctx, *args, **kwargs):
             print('Status ', r.status_code)
             print(data)
         else:
+            loadbalancers = []
             items = data['response']['items']
             for x in items:
-                loadBalancers = x['compute']['launchSpecification']['loadBalancers']
-            #print(loadBalancers)
+                loadbalancers = x['compute']['launchSpecification']['loadBalancers']
+            if debug:
+                print("Ocean LoadBalancer List: ", loadbalancers)
 
-            newloadBalancers = []
-            for x in loadBalancers:
-                if x['arn'] == loadBalancerarn:
+            newloadbalancers = []
+            for x in loadbalancers:
+                if x['arn'] == loadbalancerarn:
                     pass
                 else:
-                    newloadBalancers.append(x)
-            #print(newloadBalancers)
+                    newloadbalancers.append(x)
+            if debug:
+                print("New LoadBalancer List: ", newloadbalancers)
             headers = {
                 'Content-Type': 'application/json',
                 'Authorization': 'Bearer ' + token
             }
             url = 'https://api.spotinst.io/ocean/aws/k8s/cluster/' + cluster_id + '?accountId=' + account_id
-            data = {"cluster":{"compute":{"launchSpecification":{"loadBalancers":newloadBalancers}}}}
+            data = {"cluster": {"compute": {"launchSpecification": {"loadBalancers": newloadbalancers}}}}
 
             try:
                 r = requests.put(url, headers=headers, json=data)
                 data = json.loads(r.text)
                 if r.status_code != 200:
-                    print('Status ', r.status_code)
+                    print('Status ', r.status_code, ' Failed to remove LoadBalancer')
                     print(data)
                 else:
-                    print('Status ', r.status_code)
+                    print('Status ', r.status_code, ' Successfully removed LoadBalancer from Ocean cluster')
             except Exception as e:
                 print(e)
     except Exception as e:
